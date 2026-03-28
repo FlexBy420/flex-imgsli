4d121200b013a319f176a16a5115b809356d011c
FlexBy420 <flexby420@gmail.com>
2026-03-29 00:15:57 +0100

    new images

--- config.js
+++ config.js
@@ -18,9 +18,31 @@ const CONFIG = {
             ext: 'png',
             scenes: [
                 {
-                    name: 'The Search for El Dorado (Resolution No AA)',
+                    name: 'The Search for El Dorado (Resolution No MSAA)',
                     folder: '1',
                     labels: ['720', '2160']
+                }
+            ]
+        },
+        {
+            id: 'rcqfb',
+            name: 'Ratchet & Clank Future: Quest for Booty',
+            ext: 'png',
+            scenes: [
+                {
+                    name: 'Scene 1 (Anisotropic Filter)',
+                    folder: '2',
+                    labels: ['auto', 'x16']
+                },
+                {
+                    name: 'Scene 1 (Resolution with MSAA)',
+                    folder: '1',
+                    labels: ['100%', '300%']
+                },
+                {
+                    name: 'Scene 1 (Resolution without MSAA)',
+                    folder: '3',
+                    labels: ['100%', '300%']
                 }
             ]
         }
